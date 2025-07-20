fe2d42962dbb7f9c31bd7a7d30b5b97b1529b346
y-a-s-h-S <yashsingh2903@gmail.com>
2025-07-20 11:01:54 +0530

    added code for creating new project on backend

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -2,7 +2,7 @@ import util from 'util';
 import child_process from 'child_process';
 import fs from 'fs/promises';
 import uuid4 from 'uuid4';
-const execpro=util.promisify(child_process.exec);
+const execpromise=util.promisify(child_process.exec);
 
 export const createProjectController=async (req,res)=>{
 //create a unique id and then inside the projects folder create a new folder with that id
@@ -11,7 +11,7 @@ const projectId=uuid4();
 console.log("New project id: ",projectId);
 await fs.mkdir(`projects/${projectId}`);
 //after this call the npm create vite command in the newly created project folder
-const respnse=await execpro( `npx --yes create-vite@latest ${projectId} --template react`,{
+const response=await execpromise( `npx --yes create-vite@latest ${projectId} --template react`,{
     cwd:`projects/${projectId}`
 })
 return res.json({message:'project created' , data:projectId});
--- frontend/src/apis/ping.js
+++ frontend/src/apis/ping.js
@@ -2,7 +2,7 @@ import axios from "../config/axiosConfig";
 
 export const pingApi = async () => {
   try {
-    const response = await axios.get("/api/v1/ping");
+   const response = await axios.get("/api/v1/ping");
    console.log(response.data);
     return response.data;
   } catch (error) {
